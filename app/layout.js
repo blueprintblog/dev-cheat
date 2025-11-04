35e41941acabe037d9e3059c089cb774c2efc22f
blueprintlabs <blueprintlabs@outlook.com>
2025-11-03 22:30:50 -0300

    feat: Add Microsoft Clarity analytics script for production environment

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {process.env.NODE_ENV === 'production' && (
+          <script type="text/javascript">
+            {`(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "u0lnepd7yz");`}
+          </script>
+        )}
+      </head>
       <body
         className={`${firaCode.variable} ${nunitoSans.variable} ${jetbrainsMono.variable}  antialiased`}>
         <div className="flex flex-col min-h-screen">
